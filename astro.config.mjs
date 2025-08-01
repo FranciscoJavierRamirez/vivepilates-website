669dea133465f70cebd3561a50c92c80c293c770
Frank <89029955+FranciscoJavierRamirez@users.noreply.github.com>
2025-08-01 06:27:46 -0400

    Despliegue en github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // Para sitios estáticos en Cloudflare Pages
+  output: 'static', // Para sitios estáticos
+  site: 'https://FranciscoJavierRamirez.github.io', // Tu usuario de GitHub
+  base: '/vivepilates-website', // Nombre del repositorio
   integrations: [mdx(), react()],
   vite: {
     plugins: [tailwindcss()],
