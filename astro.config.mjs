37972188b6d32cc52fe778cbc9d8e529fc80c79c
Frank <89029955+FranciscoJavierRamirez@users.noreply.github.com>
2025-08-01 05:04:29 -0400

    Configuraciones para desplegar en CloudFlare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // Para sitios estáticos en Cloudflare Pages
   integrations: [mdx(), react()],
   vite: {
     plugins: [tailwindcss()],
